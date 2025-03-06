cdf599c32e9ea1f37c121bae929fbe3cbf73e456
yoolri <lucca6042@gmail.com>
2025-03-06 13:01:15 +0900

    [Refactor] diaries.go 변수명 수정

--- pkg/services/migrations/mig/diaries.go
+++ pkg/services/migrations/mig/diaries.go
@@ -7,7 +7,7 @@ import (
 
 // AddDiariesMigrations 그림일기 관련 마이그레이션 추가
 func AddDiariesMigrations(mg *migrator.Migrator) {
-	DiariesTable := schema.Table{  
+	diariesTable := schema.Table{  
 		Name: "diaries",
 		Columns: []*schema.Column{
 			{Name: "uid", Type: schema.DB_NVarchar, Length: 50, Nullable: false, IsPrimaryKey: true},
@@ -24,10 +24,10 @@ func AddDiariesMigrations(mg *migrator.Migrator) {
 	}
 
 	// 테이블 생성 마이그레이션 추가
-	mg.AddMigration(migrator.NewAddTableMigration(DiariesTable))
+	mg.AddMigration(migrator.NewAddTableMigration(diariesTable))
 
 	// 인덱스 추가 마이그레이션 추가
-	mg.AddMigration(migrator.NewAddIndexMigration(DiariesTable, *DiariesTable.Indices[0]))
+	mg.AddMigration(migrator.NewAddIndexMigration(diariesTable, *diariesTable.Indices[0]))
 
 	// 외래 키 추가 마이그레이션
 	mg.AddMigration(migrator.NewRawSQLMigration(
